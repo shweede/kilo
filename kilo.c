9db88ab6556553ac6ac939436d1df3c860000505
Mārcis <shweede@gmail.com>
2019-10-16 19:25:38 +0300

    Add TODO, add window size getter

--- kilo.c
+++ kilo.c
@@ -1,15 +1,26 @@
+/*
+    TODO:
+    - get rid of global data - totally unnecessary
+    - split into multiple files instead of sections
+    - give magic numbers meaningful names
+    - review style
+*/
+
 /*** includes ***/
 
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** data ***/
 
 struct editorConfig {
+    int screenRows;
+    int screenCols;
     struct termios orig_termios;
 };
 
@@ -74,6 +85,19 @@ char editorReadKey()
     return c;
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
 
 void editorDrawRows()
@@ -105,9 +129,16 @@ void editorProcessKeypress()
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenRows, &E.screenCols) == -1) {
+        die("getWindowSize");
+    }
+}
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
